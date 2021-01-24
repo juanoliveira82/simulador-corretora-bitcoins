4a5f40dce2444c4f20b1950344d79fd43c786132
Juan Carlos Cardoso de Oliveira <juanoliveira82@outlook.com>
2021-01-23 23:39:03 -0300

    Update Transacoes.java
    Mudança de nomes dos investidores de exemplo.

--- SistemaFinanceiro/src/SistemaFinanceiro/Transacoes.java
+++ SistemaFinanceiro/src/SistemaFinanceiro/Transacoes.java
@@ -8,8 +8,8 @@ public class Transacoes {
         // Cria o objeto da corretora.
         Corretora cor = new Corretora();        
         // Cria dois investidores para comprar bitcoins.
-        Investidor inv1 = new Investidor(cor, "LUCIANO DO YOUTUBE");
-        Investidor inv2 = new Investidor(cor, "MC ZOI DE GATO");
+        Investidor inv1 = new Investidor(cor, "JUAN OLIVEIRA");
+        Investidor inv2 = new Investidor(cor, "GABRIEL HARUKI");
         // Inicia as transações dos investidores
         inv1.start();
         inv2.start();
